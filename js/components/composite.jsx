9d01b50c3215a51db883fe63ec334b98ec1c3894
Andrea Ferretti <ferrettiandrea@gmail.com>
2015-02-20 15:45:47 +0100

    Removed unused code

--- js/components/composite.jsx
+++ js/components/composite.jsx
@@ -6,21 +6,7 @@ define([
   './panel',
   './pie'
 ], function(React, Row, Search, List, Panel, Pie) {
-  var countries = [
-    { name: 'Italy', population: 59859996 },
-    { name: 'Mexico', population: 118395054 },
-    { name: 'France', population: 65806000 },
-    { name: 'Argentina', population: 40117096 },
-    { name: 'Japan', population: 127290000 }
-  ];
-
   return React.createClass({
-    mixins: [React.addons.LinkedStateMixin],
-    getInitialState: function() {
-      return {
-        value: ''
-      };
-    },
     render: function() {
       return (
         <div className="container">
